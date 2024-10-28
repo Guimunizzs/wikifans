d8d1d4a0fe54abde66330850ad3ca09d072ebeb2
Guilherme Muniz <gui.munizalemao@gmail.com>
2024-10-28 19:50:44 -0300

    alterando vite

--- wikifans/vite.config.js
+++ wikifans/vite.config.js
@@ -4,5 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/wikifans/", // Define o caminho base para GitHub Pages
 });
